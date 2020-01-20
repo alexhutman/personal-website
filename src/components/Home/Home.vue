46c1f2bfa116769adff0498bb8891797fd4f4f7f
Alex <alexhutman@gmail.com>
2020-01-20 02:00:41 -0500

    Added fontawesome

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <font-awesome-icon icon="user-secret" />
+  </div>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
