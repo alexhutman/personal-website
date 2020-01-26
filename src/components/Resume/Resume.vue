1d432a168d2a1ccccbd87b36b78834730a76ef79
Alex <alexhutman@gmail.com>
2020-01-26 00:46:24 -0500

    bound resume url for easier manipulation
    also added easy option for if you wanted the google embed or the standard embed

--- src/components/Resume/Resume.vue
+++ src/components/Resume/Resume.vue
@@ -2,20 +2,31 @@
   <div class="fill-page resume-container container-fluid">
     <div class="row text-center">
       <div class="col-lg-12 embed-responsive">
-        <iframe
-          class="embed-responsive-item"
-          id="resume"
-          src="http://docs.google.com/gview?url=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf&embedded=true"
-        ></iframe>
+        <iframe class="embed-responsive-item" id="resume" :src="getResumeUrl(true)"></iframe>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import Vue from 'vue';
+
+export default Vue.extend({
   name: 'Resume',
-};
+  data() {
+    return {
+      resumeURL:
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+    };
+  },
+  methods: {
+    getResumeUrl(googleEmbed: boolean): string {
+      return googleEmbed
+        ? `http://docs.google.com/gview?url=${this.$data.resumeURL}&embedded=true`
+        : this.$data.resumeURL;
+    },
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
