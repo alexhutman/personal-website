3d9af958c262e11c2cca79d64c514c27b5696bdf
Alex <alexhutman@gmail.com>
2020-01-29 22:22:58 -0500

    added skeleton layout of aes page

--- src/components/Projects/AES/Aes.vue
+++ src/components/Projects/AES/Aes.vue
@@ -1,7 +1,44 @@
 <template>
   <div class="fill-page">
     <div class="page-container aes-container">
-        Geegee googoo gaga
+      <div class="container-fluid">
+        <div class="row welcome text-center">
+          <div class="col-12">
+            <h1 class="display-4">Advanced Encryption Standard (AES)</h1>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="explanation col-md-6 text-center">
+              Explanation
+          </div>
+
+          <div class="interactive col-md-6">
+              <div class="input">
+                  <div class="row message text-center">
+                    <div class="col text-center">
+                        Msg
+                    </div>
+                  </div>
+
+                  <div class="row key">
+                    <div class="col text-center">
+                        Key
+                    </div>
+                  </div>
+              </div>
+
+              <div class="row state">
+                <div class="col text-center">
+                    State
+                </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
