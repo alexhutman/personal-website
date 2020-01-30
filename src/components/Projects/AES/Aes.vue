9271922cb679cb7b80d0208b820636ce4702c6db
Alex <alexhutman@gmail.com>
2020-01-29 22:58:58 -0500

    added basic matrix for the state

--- src/components/Projects/AES/Aes.vue
+++ src/components/Projects/AES/Aes.vue
@@ -32,7 +32,24 @@
 
               <div class="row state">
                 <div class="col text-center">
-                    State
+                    <div class="state-grid">
+                        <div class="cell-00">00</div>
+                        <div class="cell-01">01</div>
+                        <div class="cell-02">02</div>
+                        <div class="cell-03">03</div>
+                        <div class="cell-10">10</div>
+                        <div class="cell-11">11</div>
+                        <div class="cell-12">12</div>
+                        <div class="cell-13">13</div>
+                        <div class="cell-20">20</div>
+                        <div class="cell-21">21</div>
+                        <div class="cell-22">22</div>
+                        <div class="cell-23">23</div>
+                        <div class="cell-30">30</div>
+                        <div class="cell-31">31</div>
+                        <div class="cell-32">32</div>
+                        <div class="cell-33">33</div>
+                    </div>
                 </div>
               </div>
           </div>
