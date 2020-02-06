d3ed94c5f49811788daabe127439bec8ef5fa529
Alex <alexhutman@gmail.com>
2020-02-05 20:26:08 -0500

    more progress on aes

--- src/components/Projects/AES/AES.ts
+++ src/components/Projects/AES/AES.ts
@@ -4,15 +4,38 @@ export default class AES {
   private N_b = 4;
   private keyLength: number;
 
-  private N_k: number;
-  private N_r: number;
+  private N_k!: number;
+  private N_r!: number;
 
-  private keySchedule: number[];
+  private keySchedule!: number[];
 
   constructor(key_len: number) {
     this.keyLength = key_len as keyRange;
   }
 
+  private generateRoundConsts(): number[] {
+    const rc: number[] = [];
+
+    this.range(10).forEach((i) => {
+      if (i == 0) {
+        rc.push(1);
+      } else if (i > 0 && rc[i-1] < 0x80) {
+        rc.push(2 * rc[i-1]);
+      } else if (i > 0 && rc[i - 1] >= 0x80) {
+        rc.push(((2 * rc[i-1]) ^ 0x1B) & 0xFF);
+      }
+    });
+
+    return rc;
+  }
+
+  private numberDictKeys(dict: Record<string, Number>): number[] {
+    return Object.keys(dict).map((str) => Number(str));
+  }
+
+  private range(n: number): number[] {
+    return [...Array(n).keys()];
+  }
   public encrypt(msg: string[], key: string[]): string {
     this.validateKey(key, this.keyLength);
     [this.N_k, this.N_r] = this.calculateConstants(key, this.keyLength);
@@ -52,7 +75,7 @@ export default class AES {
     return "".join([hexify_state(enc_block) for enc_block in encrypted_blocks])
   }
 
-  private validateKey(key: string[], keyLen: number): void {
+  private validateKey(key: number[], keyLen: number): void {
     const numRounds = {
       128: 10,
       192: 12,
@@ -69,8 +92,8 @@ export default class AES {
     }
   }
 
-  private calculateConstants(key: string[], keyLen: number): number[] {
-    const numRounds = {
+  private calculateConstants(key: number[], keyLen: number): number[] {
+    const numRounds: { [keyLen: number]: number } = {
       128: 10,
       192: 12,
       256: 14
@@ -81,50 +104,26 @@ export default class AES {
     return [N_k, N_r];
   }
 
-  // private generateKeySchedule(key: string[]): string[] {
-  //   const rCon = this.generateRoundConsts();
+  private generateKeySchedule(key: number[]): number[] {
+    const rCon = this.generateRoundConsts();
 
-  //   const w = [];
-  //   for i in range(4*(self.N_r+1)): #TODO: change 11 to num_round keys needed
-  //       #print(f"i = {i}")
-  //       if i < self.N_k:
-  //           print("Case 1")
-  //           w.append(key[4*i:4*i+4])
-  //       elif i >= self.N_k and i % self.N_k == 0:
-  //           print("Case 2")
-  //           w.append(Encrypt.__xor_col(w[i - self.N_k], Encrypt.__transform_col(w[i - 1], Rcon[(i // self.N_k) - 1])))
-  //       elif i >= self.N_k and self.N_k > 6 and i % self.N_k == 4:
-  //           print("Case 3")
-  //           w.append(Encrypt.__xor_col(w[i - self.N_k], [lookups.s_box[x] for x in w[i-1]]))
-  //       else:
-  //           print("Case 4")
-  //           w.append(Encrypt.__xor_col(w[i - self.N_k], w[i-1]))
-  //       #print(f"w[{i}] = {''.join([hex(a)[2:].rjust(2, '0') for a in w[i]])}")
-
-  //   return w
-  // }
-
-  private generateRoundConsts(): number[] {
-    const rc: number[] = [];
-
-    this.range(10).forEach((i) => {
-      if (i == 0) {
-        rc.push(1);
-      } else if (i > 0 && rc[i-1] < 0x80) {
-        rc.push(2 * rc[i-1]);
-      } else if (i > 0 && rc[i - 1] >= 0x80) {
-        rc.push(((2 * rc[i-1]) ^ 0x1B) & 0xFF);
-      }
+    const w = [];
+    this.range(4*(self.N_r+1)).forEach((i) => {
+      if i < self.N_k:
+          print("Case 1")
+          w.append(key[4*i:4*i+4])
+      elif i >= self.N_k and i % self.N_k == 0:
+          print("Case 2")
+          w.append(Encrypt.__xor_col(w[i - self.N_k], Encrypt.__transform_col(w[i - 1], Rcon[(i // self.N_k) - 1])))
+      elif i >= self.N_k and self.N_k > 6 and i % self.N_k == 4:
+          print("Case 3")
+          w.append(Encrypt.__xor_col(w[i - self.N_k], [lookups.s_box[x] for x in w[i-1]]))
+      else:
+          print("Case 4")
+          w.append(Encrypt.__xor_col(w[i - self.N_k], w[i-1]))
+      #print(f"w[{i}] = {''.join([hex(a)[2:].rjust(2, '0') for a in w[i]])}")
     });
 
-    return rc;
-  }
-
-  private numberDictKeys(dict: Record<string, Number>): number[] {
-    return Object.keys(dict).map((str) => Number(str));
-  }
-
-  private range(n: number): number[] {
-    return [...Array(n).keys()];
+    return w
   }
 }
