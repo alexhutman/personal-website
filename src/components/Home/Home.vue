a753d26b3ba3d30b54f37682ab6f1d9f764e3538
Alex <alexhutman@gmail.com>
2020-05-28 20:46:48 -0400

    added 3rd transition to home
    will eventually become an actual page saying like 'hi click here for projects, here for about me, here for my resume, etc'

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="home-container">
+    <div id="logo" class="logo-container d-flex flex-row justify-content-center no-select">
+        <div class="logo alex no-select">
+          alex
+        </div>
+
+        <div class="logo hutman no-select">
+          hutman
+        </div>
+    </div>
+
     <div id="quote" class="d-none mx-2 no-select">
       <blockquote class="lead text-white blockquote text-center">
         <p class="mb-0">
@@ -11,14 +21,9 @@
         </footer>
       </blockquote>
     </div>
-    <div id="logo" class="logo-container d-flex flex-row justify-content-center no-select">
-        <div class="logo alex no-select">
-          alex
-        </div>
 
-        <div class="logo hutman no-select">
-          hutman
-        </div>
+    <div id="home-page" class="d-none no-select">
+      Hello :)
     </div>
 
   </div>
@@ -29,18 +34,29 @@ export default {
   name: 'Home',
   data() {
     return {
-      fadeInDelay: 5, // in seconds
+      logoFadeInDuration: 5, // in seconds
+      quoteFadeInDuration: 6, // in seconds
     };
   },
   mounted() {
     const logo = document.getElementById('logo');
     const quote = document.getElementById('quote');
+    const homePage = document.getElementById('home-page');
+
+    const logoDelayMs = (this as any).logoFadeInDuration * 1000 + 50;
+    const homePageDelayMs = (this as any).quoteFadeInDuration * 1000 + 50;
 
     setTimeout(() => {
       logo!.remove();
       quote!.classList.remove('d-none');
       quote!.classList.add('quote-anim');
-    }, (this as any).fadeInDelay * 1000 + 50); //  5s * 1000ms/s + 50 ms to be safe
+
+      setTimeout(() => {
+        quote!.remove();
+        homePage!.classList.remove('d-none');
+        homePage!.classList.add('home-page-anim');
+      }, homePageDelayMs); //  5s * 1000ms/s + 50 ms to be safe
+    }, logoDelayMs); //  5s * 1000ms/s + 50 ms to be safe
   },
 };
 </script>
