b3bc16e36f3b5bfed99297b427ac9d7bfd0df3db
Alex <alexhutman@gmail.com>
2020-07-03 23:54:58 -0400

    added header-page component to Projects page

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,53 +1,55 @@
 <template>
-  <div class="projects-container">
-    <div class="container-fluid">
-      <div class="row welcome text-center">
-        <div class="col-12">
-          <h1 class="display-4">Projects</h1>
-          <hr />
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="card">
-            <img class="card-img-top" src="assets/img/aes.png" />
-            <div class="card-body">
-              <h4 class="card-title">AES</h4>
-              <p class="card-text">This is the description for AES xd</p>
-              <button class="btn btn-outline-secondary">
-                <router-link to="/projects/aes">See Project</router-link>
-              </button>
+  <header-page :page-title="title">
+    <div class="projects-container">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <img class="card-img-top" src="assets/img/aes.png" />
+              <div class="card-body">
+                <h4 class="card-title">AES</h4>
+                <p class="card-text">This is the description for AES xd</p>
+                <button class="btn btn-outline-secondary">
+                  <router-link to="/projects/aes">See Project</router-link>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-md-6">
-          <div class="card">
-            <img
-              class="card-img-top"
-              src="assets/gaussian/gaussian-main.png"
-            />
-            <div class="card-body">
-              <h4 class="card-title">Gaussian</h4>
-              <p class="card-text">This is the description for Gaussian xd</p>
-              <button class="btn btn-outline-secondary">
-                <router-link to="/projects/gaussian">See Project</router-link>
-              </button>
+          <div class="col-md-6">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="assets/gaussian/gaussian-main.png"
+              />
+              <div class="card-body">
+                <h4 class="card-title">Gaussian</h4>
+                <p class="card-text">This is the description for Gaussian xd</p>
+                <button class="btn btn-outline-secondary">
+                  <router-link to="/projects/gaussian">See Project</router-link>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </header-page>
 </template>
 
 <script lang="ts">
+import HeaderPage from '../HeaderPage/HeaderPage.vue';
+
 export default {
   name: 'Projects',
-  methods: {},
+  data() {
+    return {
+      title: 'Projects',
+    };
+  },
+  components: {
+    HeaderPage,
+  },
 };
 </script>
 
