481ede8374df68106f24f18156b880e4cfdd3beb
Alex <alexhutman@gmail.com>
2020-08-17 23:48:48 -0400

    contact cells are more centered

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -5,34 +5,34 @@
         <!-- I think instead of text at all, I just want to have clickable icons that link to
         respective ways to contact me: i.e. email, linkedin, etc. -->
         <div class="contact-text text-center row">
-          <a href="mailto:alex@alexhutman.com" class="col-md-6 align-middle contact-link mail pt-3"
+          <a href="mailto:alex@alexhutman.com" class="col-md-6 contact-link mail py-3"
           target="_blank">
             <div class="text-center">
-                <font-awesome-icon :icon="['fa', 'envelope']" class="fa-3x icon"/>
+                <font-awesome-icon :icon="['fa', 'envelope']" class="fa-3x icon align-middle"/>
                 alex@alexhutman.com
             </div>
           </a>
 
           <a href="mailto:alex@alexhutman.com?subject=Phone%20Number
           &body=Hi%20Alex,%20please%20give%20me%20your%20phone%20number!"
-          class="col-md-6 contact-link phone pt-3"
+          class="col-md-6 contact-link phone py-3"
           target="_blank">
             <div>
-                <font-awesome-icon :icon="['fa', 'phone']" class="fa-3x icon"/>
+                <font-awesome-icon :icon="['fa', 'phone']" class="fa-3x icon align-middle"/>
                 Please email for phone number
             </div>
           </a>
 
-          <a href="https://github.com/alexhutman" class="col-md-6 contact-link github pt-3" target="_blank">
+          <a href="https://github.com/alexhutman" class="col-md-6 contact-link github py-3" target="_blank">
             <div>
-              <font-awesome-icon :icon="['fab', 'github-square']" class="fa-3x icon"/>
+              <font-awesome-icon :icon="['fab', 'github-square']" class="fa-3x icon align-middle"/>
                 alexhutman
             </div>
           </a>
 
-          <a href="https://linkedin.com/in/alexhutman" class="col-md-6 contact-link linkedin pt-3" target="_blank">
+          <a href="https://linkedin.com/in/alexhutman" class="col-md-6 contact-link linkedin py-3" target="_blank">
             <div>
-              <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-3x icon"/>
+              <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-3x icon align-middle"/>
                 alexhutman
             </div>
           </a>
