0135b449b7e76d05d1f3ed4c56f2b0a89fd994f0
Alex <alexhutman@gmail.com>
2020-08-18 02:12:05 -0400

    removed all fontawesome vue stuff

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import { library, IconDefinition } from '@fortawesome/fontawesome-svg-core';
-
-import {
-  faBars, faCommentDots, faKey, faEnvelope, faPhone,
-} from '@fortawesome/free-solid-svg-icons';
-import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const solidIcons: IconDefinition[] = [faBars, faCommentDots, faKey, faEnvelope, faPhone];
-const brandIcons: IconDefinition[] = [faGithubSquare, faLinkedin];
-
-[...solidIcons, ...brandIcons].map(icon => library.add(icon));
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
