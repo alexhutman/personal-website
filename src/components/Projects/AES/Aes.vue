01a321c9897f1843ced699d72a96a71fd37496fb
Alex <alexhutman@gmail.com>
2020-09-15 23:52:37 -0400

    removed left/right arrows on projects slides

--- src/components/Projects/AES/Aes.vue
+++ src/components/Projects/AES/Aes.vue
@@ -504,16 +504,6 @@ return w
                     </div>
                   </div>
                 </div>
-                <a class="carousel-control-prev" href="#aesCarousel"
-                  role="button" v-on:click="onPrevSlide()" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#aesCarousel"
-                  role="button" v-on:click="onNextSlide()" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
               </div>
             </div>
           </div>
