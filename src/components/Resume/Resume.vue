e780f8f440633c82b2ab275268129e814d501d9d
Alex <alexhutman@gmail.com>
2020-09-17 16:15:56 -0400

    new resume

--- src/components/Resume/Resume.vue
+++ src/components/Resume/Resume.vue
@@ -2,7 +2,7 @@
   <div class="resume-container">
     <div class="alert alert-warning text-center" role="alert">
       If the links aren't clickable, please click
-      <a class="alert-link" href="alex_hutman_resume_sept_16_2020.pdf" target="_blank">here</a>
+      <a class="alert-link" href="alex_hutman_resume_sept_17_2020.pdf" target="_blank">here</a>
       to open my resume in a new tab and they will be clickable.
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
@@ -12,11 +12,11 @@
     <div class="container">
       <div class="embed-responsive pdf-aspect">
         <object class="embed-responsive-item"
-        data="alex_hutman_resume_sept_16_2020.pdf"
+        data="alex_hutman_resume_sept_17_2020.pdf"
         type="application/pdf">
           <p>
             Your browser doesn't support embedded PDF files. You can download the file
-            <a href="alex_hutman_resume_sept_16_2020.pdf">here</a>.
+            <a href="alex_hutman_resume_sept_17_2020.pdf">here</a>.
           </p>
         </object>
       </div>
