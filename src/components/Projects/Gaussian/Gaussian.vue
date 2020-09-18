208d733fc1f3c181e526f36f96a144011fc2dd0f
Alex <alexhutman@gmail.com>
2020-09-17 20:04:39 -0400

    reveal js concept on gaussian

--- src/components/Projects/Gaussian/Gaussian.vue
+++ src/components/Projects/Gaussian/Gaussian.vue
@@ -3,20 +3,13 @@
       <div class="container-fluid">
         <div class="row">
           <!-- <div class="explanation col-md-4 text-center"> -->
-          <div class="explanation col-md-4 text-center vertical-center">
-            <div class="container-fluid">
-              <div id="gaussianCarousel" class="carousel slide" data-ride="carousel"
-                data-interval="false">
-                <ol class="carousel-indicators">
-                  <li data-target="#gaussianCarousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#gaussianCarousel" data-slide-to="1"></li>
-                  <li data-target="#gaussianCarousel" data-slide-to="2"></li>
-                  <li data-target="#gaussianCarousel" data-slide-to="3"></li>
-                  <li data-target="#gaussianCarousel" data-slide-to="4"></li>
-                </ol>
-                <div class="carousel-inner" style=" width:100%; height: 500px !important;">
-                  <div class="carousel-item active">
-                    <img src="assets/gaussian/matrix.png" class="img-fluid fit-carousel-img"
+          <div class="explanation col-md-4" style="width: 100vw !important;
+          height: 78vh !important;">
+              <div id="gaussianCarousel" class="reveal">
+                  <div class="slides">
+                    <section>
+                    <h5>How does it work?</h5>
+                      <img src="assets/gaussian/matrix.png" class="img-fluid"
                       alt="...">
                     <!--
                       <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100"
@@ -28,19 +21,18 @@
                       <text x="50%" y="50%" fill="#333" dy=".3em">First slide</text>
                     </svg>
                     -->
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>How does it work?</h5>
                       <p>
                         First we start with a RADIUSxRADIUS matrix. For example, the above is a
                         matrix where the radius is equal to 3. The middle entry is the current pixel
                         that we're looking at. We then plug each of these (x,y) values into the
                         following equation to get a weighted matrix:
                       </p>
-                    </div>
+                    </section>
+                    <section>Jeff</section>
                   </div>
+                  <!--
                   <div class="carousel-item">
                     <img src="assets/gaussian/equation.png" class="img-fluid" alt="...">
-                    <!--
                     <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
                     height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid
                     slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
@@ -48,9 +40,7 @@
                     <rect width="100%" height="100%" fill="#555"></rect>
                     <text x="50%" y="50%" fill="#333" dy=".3em">Second slide</text>
                     </svg>
-                    -->
                     <div class="carousel-caption d-none d-md-block">
-                      <!-- <h5>Second slide label</h5> -->
                       <p class="carousel-text">
                         This is the 3D version of a normalized bell curve that has a standard
                         deviation of σ. First, picture a 2D bell curve. If we make it have a large
@@ -65,7 +55,6 @@
                   <div class="carousel-item">
                     <img src="assets/gaussian/matrix2.png" class="img-fluid fit-carousel-img"
                       alt="...">
-                    <!--
                     <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
                     height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid
                     slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
@@ -73,9 +62,7 @@
                     <rect width="100%" height="100%" fill="#555"></rect>
                     <text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text>
                     </svg>
-                    -->
                     <div class="carousel-caption d-none d-md-block">
-                      <!-- <h5>Third slide label</h5> -->
                       <p class="carousel-text">
                         We are almost done! We must now normalize the matrix by calculating the sum
                         of every value and then divide each entry by this sum. If we did not do
@@ -87,7 +74,6 @@
                   <div class="carousel-item">
                     <img src="assets/gaussian/matrix3.png" class="img-fluid fit-carousel-img"
                       alt="...">
-                    <!--
                     <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
                     height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid
                     slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
@@ -95,9 +81,7 @@
                     <rect width="100%" height="100%" fill="#555"></rect>
                     <text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text>
                     </svg>
-                    -->
                     <div class="carousel-caption d-none d-md-block">
-                      <!-- <h5>Third slide label</h5> -->
                       <p class="carousel-text">
                         Now we must weigh each pixel according to this matrix. This means the
                         center pixel's new R value is (top-left pixel's R value)*(0.0947416) +
@@ -109,7 +93,6 @@
                   <div class="carousel-item">
                     <img src="assets/gaussian/matrix3.png" class="img-fluid fit-carousel-img"
                       alt="...">
-                    <!--
                     <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
                     height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid
                     slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
@@ -117,24 +100,19 @@
                     <rect width="100%" height="100%" fill="#555"></rect>
                     <text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text>
                     </svg>
-                    -->
                     <div class="carousel-caption d-none d-md-block">
-                      <!-- <h5>Third slide label</h5> -->
                       <p class="carousel-text">
                         We're done! Each pixel is averaged according to the pixels around it,
                         producing a Gaussian blur. As you can see, σ = 1.5 and a radius size of 3
                         does not produce a very intense blur. Try changing the σ and Radius values
                         to get a blurrier image!
-                        <!--
                           You can also upload your own picture and blur it by hitting the Choose
                           File button!
-                        -->
                       </p>
                     </div>
                   </div>
-                </div>
+              -->
               </div>
-            </div>
           </div>
 
           <div class="interactive col-md-8 text-center">
@@ -178,6 +156,9 @@ import GaussianBlur from './GaussianBlur';
 
 import HeaderPage from '../../HeaderPage/HeaderPage.vue';
 
+// @ts-ignore
+import Reveal from '../../../../node_modules/reveal.js/dist/reveal';
+
 export default Vue.extend({
   name: 'Gaussian',
   data() {
@@ -189,9 +170,12 @@ export default Vue.extend({
   },
   mounted() {
     this.gb = new GaussianBlur(this.kernelSliderValue);
+
+    Reveal.initialize({
+      embedded: true,
+    });
   },
-  methods: {
-  },
+  methods: {},
   components: {
     HeaderPage,
   },
@@ -199,5 +183,4 @@ export default Vue.extend({
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src="./Gaussian.scss" lang="scss">
-</style>
+<style scoped src="./Gaussian.scss" lang="scss"></style>
